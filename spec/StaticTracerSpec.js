bf6b6c3daa2b2e2cf5d34678b31e1c4ba766d7db
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2015-03-03 17:02:36 +0100

    Can extract multiple variables if each is declared on its own

--- spec/StaticTracerSpec.js
+++ spec/StaticTracerSpec.js
@@ -6,7 +6,7 @@ describe("StaticTracer", function() {
     });
 
     describe("when given a line of code that assigns a literal to a variable", function() {
-        it("returns an object that indexes the literal by its variable name", function() {
+        it("returns an object with the literal accessible by its variable name", function() {
             expect(testTracer.trace("var something = [4, 3, 6];")).toEqual({something: [4, 3, 6]});
             expect(testTracer.trace("var someInt = 6;").someInt).toEqual(6);
             expect(testTracer.trace("var someObj = {foo: 'bar'};").someObj).toEqual({foo: "bar"});
@@ -18,6 +18,25 @@ describe("StaticTracer", function() {
     describe("when given a line of code with no variable assignment", function() {
         it("sets the variable to null", function() {
             expect(testTracer.trace("var someNothing;").someNothing).toEqual(null);
+        });
+    });
+
+    describe("when given multiple literal assignments", function() {
+        it("returns an object with the literals accessible by their variable names", function() {
+            var source = "var someInt = 42;\nvar someString = 'asdf';\nvar someArray = ['a', 'b', 'c'];\n" +
+                         "var someObj = {bar : 'baz'}";
+            expect(testTracer.trace(source).someInt).toEqual(42);
+            expect(testTracer.trace(source).someString).toEqual("asdf");
+            expect(testTracer.trace(source).someArray).toEqual(['a', 'b', 'c']);
+            expect(testTracer.trace(source).someObj).toEqual({ bar: "baz" });      
+        });
+    });
+
+    xdescribe("when given an assignment of a previously initialized variable", function() {
+        it("assigns the new variable with the literal of the other variable", function() {
+            var source = "var first = 'x';\nvar second = first;";
+            expect(testTracer.trace(source).first).toEqual('x');
+            expect(testTracer.trace(source).second).toEqual('x');
         });
     });
 });
--- src/StaticTracer.js
+++ src/StaticTracer.js
@@ -2,36 +2,42 @@ var esprima = require('esprima');
 
 exports.trace = function(source) {
     var parsed = esprima.parse(source);
-    //console.log(JSON.stringify(parsed, null, 2));
+    // console.log(JSON.stringify(parsed, null, 2));
     
     var returnObject = {};
-    var firstDeclaration = parsed.body[0].declarations[0];
-    var varName = firstDeclaration.id.name;
     var varValue;
-    
-    if(firstDeclaration.init == null) {
-        returnObject[varName] = null;
-        return returnObject;
-    }
-    if(firstDeclaration.init.type === "ArrayExpression") {
-        var elements = [];
-        var idx;
-        for(idx in firstDeclaration.init.elements) {
-            elements.push(firstDeclaration.init.elements[idx].value);
-        }
-        varValue = elements;
-    } else if(firstDeclaration.init.type === "ObjectExpression") {
-       var object = {};
-       var idx;
-       var props = firstDeclaration.init.properties;
-       for(idx in props) {
-           object[props[idx].key.name] = props[idx].value.value; 
-       }
-       varValue = object;
-    } else {
-        varValue = firstDeclaration.init.value;
-    }
+    var declIdx;
 
-    returnObject[varName] = varValue;
+    for(declIdx in parsed.body) {
+      var declaration = parsed.body[declIdx].declarations[0];
+      varName = declaration.id.name;
+      
+      if(declaration.init == null) {
+          returnObject[varName] = null;
+          return returnObject;
+      }
+      
+      if(declaration.init.type === "ArrayExpression") {
+          var elements = [];
+          var idx;
+          for(idx in declaration.init.elements) {
+              elements.push(declaration.init.elements[idx].value);
+          }
+          varValue = elements;
+      } else if(declaration.init.type === "ObjectExpression") {
+         var object = {};
+         var idx;
+         var props = declaration.init.properties;
+         for(idx in props) {
+             object[props[idx].key.name] = props[idx].value.value; 
+         }
+         varValue = object;
+      } else {
+          varValue = declaration.init.value;
+      }
+  
+      returnObject[varName] = varValue;
+    }
+    
     return returnObject; 
 };
