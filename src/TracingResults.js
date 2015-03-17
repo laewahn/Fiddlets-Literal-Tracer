1fd7a1298bb214029342ce4d75ab5b0336338b93
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2015-03-17 13:57:15 +0100

    Forgot to include the TracingResults module previously extracted m(

---
+++ src/TracingResults.js
@@ -0,0 +1,45 @@
+function TracingResults() {}
+
+
+TracingResults.prototype.tracedValueOf = function(variableName, line) {
+  var theLine = line;
+  var theScope = this.scopeForLine(theLine);
+  var returnValue = theScope[variableName];
+  
+  while(theScope !== undefined && (returnValue === null || returnValue === undefined)) {
+    theScope = theScope.__parentScope;
+    returnValue = theScope[variableName];
+  }
+
+  return returnValue;
+}
+
+TracingResults.prototype.scopeForLine = function(line) {
+  return findScopeWith(function(scope) {
+    return scope.__location.start.line < line && line < scope.__location.end.line
+  }, this);
+}
+
+TracingResults.prototype.scopeByName = function(functionName) {
+  return findScopeWith(function(scope) {
+    return scope.__scopeName === functionName;
+  }, this);
+}
+
+function findScopeWith(evaluationFunction, tracingResults) {
+  var bestCandidate = null;
+  tracingResults.__scopes.forEach(function(scopeName) {
+
+    var nextScope = scopeName;
+
+    if (evaluationFunction(nextScope) === true) {
+      bestCandidate = nextScope;
+    }
+    
+    bestCandidate = findScopeWith(evaluationFunction, nextScope) || bestCandidate;
+  });
+
+  return bestCandidate;  
+}
+
+exports.TracingResults = TracingResults;
