88623aae8a988ec1185e4fac45231e5d55925b0b
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2015-03-26 09:48:09 +0100

    Implemented tracing command for domain

--- LiteralTracerDomain.js
+++ LiteralTracerDomain.js
@@ -1,8 +1,15 @@
 (function() {
 	"use strict";
 	
+	var LiteralTracer = require("./lib/LiteralTracer");
+
 	var LITERAL_TRACER_DOMAIN = "literalTracerDomain";
 	var LITERAL_TRACER_VERSION = {major : 0, minor: 1};
+
+	function traceCmd(source) {
+		var tracer = new LiteralTracer.Tracer();
+		return tracer.trace(source);
+	}
 
 	function init(domainManager) {
 		if(!domainManager.hasDomain(LITERAL_TRACER_DOMAIN)) {
@@ -10,7 +17,22 @@
 									LITERAL_TRACER_VERSION );
 		}
 
-		// register commands here.
+		domainManager.registerCommand(
+			LITERAL_TRACER_DOMAIN,
+			"trace",
+			traceCmd,
+			false,
+			"Returns the results for the literal tracer on the source",
+			[{	name: "source",
+				type: "string",
+				description: "The source to trace"
+			}],
+
+			[{	name: "tracingResult",
+				type: "object",
+				description: "TracingResult for the source"
+			}]
+		);
 	}
 
 	exports.init = init;
