0c477eb94e6fe2abc5587c3ee5b8ac08abda9db7
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2015-04-10 14:21:22 +0200

    ---

--- LiteralTracerDomain.js
+++ LiteralTracerDomain.js
@@ -13,7 +13,7 @@
 		console.log(JSON.stringify("Position: " + position));
 		var tracer = new LiteralTracer.Tracer();
 		lastTrace = tracer.trace(source).scopeForPosition(position.line, position.ch);
-		console.log(JSON.stringify(lastTrace.allAssignments(), null, 2));
+		// console.log(JSON.stringify(lastTrace.allAssignments(), null, 2));
 		
 		return lastTrace.allAssignments();
 	}
